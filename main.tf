8a446a843792eb7425d2ca384632367d9d28bf0f
Awele Ajiduah <49324667+Missawi1@users.noreply.github.com>
2023-06-09 00:12:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-resource "aws_cognito_identity_pool" "my-identity-pool" {
-  identity_pool_name               = "my-identity-pool"
-  allow_unauthenticated_identities = false
-  allow_classic_flow               = false
-
-  cognito_identity_providers {
-    client_id               = "6lhlkkfbfb4q5kpp90urffae"
-    provider_name           = "cognito-idp.us-east-1.amazonaws.com/us-east-1_Tv0493apJ"
-    server_side_token_check = false
-  }
-
-  cognito_identity_providers {
-    client_id               = "google-client-id"
-    provider_name           = "accounts.google.com"
-    server_side_token_check = true
-  }
-
-  cognito_identity_providers {
-    client_id               = "facebook-client-id"
-    provider_name           = "graph.facebook.com"
-    server_side_token_check = true
-  }
-
-  supported_login_providers = {
-    "graph.facebook.com"  = "7346241598935552"
-    "accounts.google.com" = "123456789012.apps.googleusercontent.com"
-  }
-}
-
 resource "aws_cognito_user_pool" "my-user-pool" {
   name = "my-user-pool"
   auto_verified_attributes = ["Email"]
@@ -126,39 +97,6 @@ resource "aws_cognito_user_pool_domain" "my-user-pool-domain" {
   certificate_arn = aws_acm_certificate.my-app-cert.arn
 }
 
-resource "aws_cognito_identity_provider" "Google" {
-  user_pool_id  = aws_cognito_user_pool.my-user-pool.id
-  provider_name = "Google"
-  provider_type = "Google"
-
-  provider_details = {
-    authorize_scopes = "email"
-    client_id        = "your client_id"
-    client_secret    = "your client_secret"
-  }
-
-  attribute_mapping = {
-    email    = "email"
-  }
-}
-
-resource "aws_cognito_identity_provider" "Facebook" {
-  user_pool_id  = aws_cognito_user_pool.my-user-pool.id
-  provider_name = "Facebook"
-  provider_type = "Facebook"
-
-  provider_details = {
-    authorize_scopes = "email"
-    client_id        = "your client_id"
-    client_secret    = "your client_secret"
-    api_version = "your api_version"
-  }
-
-  attribute_mapping = {
-    email    = "email"
-  }
-}
-
 resource "aws_acm_certificate" "my-app-cert" {
   domain_name       = aws_route53_record.my-app-domain.name
   validation_method   = "DNS"
@@ -172,62 +110,4 @@ resource "aws_route53_record" "my-app-domain" {
   name    = aws_cognito_user_pool_domain.my-user-pool-domain.domain
   type    = "A"
   zone_id = data.aws_route53_zone.my-app-domain.zone_id
-}
-
-resource "aws_iam_role" "authenticated" {
-  name               = "cognito_authenticated"
-  assume_role_policy = data.aws_iam_policy_document.authenticated.json
-}
-
-resource "aws_iam_role_policy" "authenticated" {
-  name   = "authenticated_policy"
-  role   = aws_iam_role.authenticated.id
-  policy = data.aws_iam_policy_document.authenticated_role_policy.json
-}
-
-resource "aws_cognito_identity_pool_roles_attachment" "main" {
-  identity_pool_id = aws_cognito_identity_pool.my-identity-pool.id
-
-  role_mapping {
-    identity_provider         = "graph.facebook.com"
-    ambiguous_role_resolution = "AuthenticatedRole"
-    type                      = "Rules"
-
-    mapping_rule {
-      claim      = "isAdmin"
-      match_type = "Equals"
-      role_arn   = aws_iam_role.authenticated.arn
-      value      = "paid"
-    }
-  }
-
-  role_mapping {
-    identity_provider         = "accounts.google.com"
-    ambiguous_role_resolution = "AuthenticatedRole"
-    type                      = "Rules"
-
-    mapping_rule {
-      claim      = "isAdmin"
-      match_type = "Equals"
-      role_arn   = aws_iam_role.authenticated.arn
-      value      = "paid"
-    }
-  }
-
-  role_mapping {
-    identity_provider         = "cognito-idp.us-east-1.amazonaws.com/us-east-1_Tv0493apJ"
-    ambiguous_role_resolution = "AuthenticatedRole"
-    type                      = "Rules"
-
-    mapping_rule {
-      claim      = "isAdmin"
-      match_type = "Equals"
-      role_arn   = aws_iam_role.authenticated.arn
-      value      = "paid"
-    }
-  }
-
-  roles = {
-    "authenticated" = aws_iam_role.authenticated.arn
-  }
 }
