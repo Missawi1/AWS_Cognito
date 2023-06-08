f9023d5c4c9e4336a62150b46475791ec1be6de7
Awele Ajiduah <49324667+Missawi1@users.noreply.github.com>
2023-06-09 00:02:38 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,15 +1,3 @@
-data "aws_route53_zone" "my_app_domain" {
-  name = "www.abc321.com"
-}
-
-data "aws_cognito_user_pool_signing_certificate" "signing_cert" {
-  user_pool_id = aws_cognito_user_pool.my-user-pool.id
-}
-
-data "external" "cognito_signing_certificate" {
-  program = ["sh", "get_cognito_signing_certificate.sh"]
-}
-
 data "aws_iam_policy_document" "authenticated" {
   statement {
     effect = "Allow"
